328471d6ff00998bebb565fca9efa93132aa4285
Joel Kaasinen <joel.kaasinen@nitor.fi>
2016-09-26 16:49:31 +0300

    first example

--- example.clj
+++ example.clj
@@ -1,3 +1,25 @@
 (ns example)
 
-(def a 1)
+(set! *warn-on-reflection* true)
+
+(definterface Foo
+  (frobnicate [])
+  (mogrify [x y]))
+
+(defn ^Foo fake-Foo []
+  (let [state-a (atom {:frobnicate 1 :data []})]
+    (reify Foo
+      (frobnicate [this]
+        (swap! state-a update-in [:frobnicate] inc))
+      (mogrify [this x y]
+        (swap! state-a update-in [:data] conj {:x x :y y})
+        (.frobnicate this)
+        :ok))))
+
+(defn ^Foo make-Foo [host port]
+  nil)
+
+(defn doit [^Foo foo times]
+  (dotimes [i times]
+    (.frobnicate foo)
+    (.mogrify foo i true)))
