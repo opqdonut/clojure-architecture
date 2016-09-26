81ba706c3f73d1e629da600bbbba69dc57abc795
Joel Kaasinen <joel.kaasinen@nitor.fi>
2016-09-26 16:38:44 +0300

    initial commit with minimal project.clj

---
+++ example.clj
@@ -0,0 +1,3 @@
+(ns example)
+
+(def a 1)
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-architecture "0.1.0-SNAPSHOT"
+  :description ""
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :source-paths ["."])
