0a7bfa56a576da7401c2d8a06ba37736ce3326b5
Joel Kaasinen <joel.kaasinen@nitor.fi>
2016-09-30 12:07:53 +0300

    add explanation to example.clj

--- example.clj
+++ example.clj
@@ -1,6 +1,14 @@
 (ns example
   (:use [clojure.string :only [split-lines]]))
 
+;; definterface doesn't generate the corresponding top-level functions
+;; like defprotocol does. You need to call (.log foo) instead of (log
+;; foo). This is nice because method calls *used to* be faster than
+;; function calls.
+;;
+;; *warn-on-reflection* gives a warning if clojure
+;; can't make the efficient direct method call but needs to use
+;; reflection to figure out which class is being called.
 (set! *warn-on-reflection* true)
 
 (definterface Counter
@@ -8,6 +16,12 @@
   (increment [n])
   (log []))
 
+;; Note: in contrast to deftype, val-r and log-r are completely
+;; hidden (private). Also, we only need one top-level definition (this
+;; function) instead of two (the deftype and the constructor function).
+;;
+;; The return type is type hinted. This acts both as documentation and makes e.g.
+;; (.log (fake-Counter)) not give a reflection warning.
 (defn ^Counter fake-Counter []
   (let [val-r (ref 0)
         log-r (ref [])]
@@ -31,6 +45,8 @@
       (increment [this n]
         (spit file (prn-str n) :append true)))))
 
+;; Note method calls instead of function calls. Type hint is again
+;; both documentation and inhibits reflection warning.
 (defn doit [^Counter c times]
   (dotimes [i times]
     (.increment c i))
